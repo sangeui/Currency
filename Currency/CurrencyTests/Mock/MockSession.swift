d07c158e6b35dd9d8e760c9b9e12d11c1cd38c49
Sangeui <Sangeuis@icloud.com>
2021-01-20 20:11:18 +0900

    test: added MockDataTask class

--- Currency/CurrencyTests/Mock/MockSession.swift
+++ Currency/CurrencyTests/Mock/MockSession.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class MockSession: URLSession {
+    private var data: Data?
+    private var error: Error?
+    private var response: HTTPURLResponse?
+    
+    init(data: Data? = nil, error: Error? = nil, response: HTTPURLResponse? = nil) {
+        self.data = data
+        self.error = error
+        self.response = response
+    }
+}
+
+class MockDataTask: URLSessionDataTask {
+    private let completion: () -> Void
+    
+    init(completion: @escaping() -> Void) {
+        self.completion = completion
+    }
+    
+    override func resume() {
+        completion()
+    }
+}
