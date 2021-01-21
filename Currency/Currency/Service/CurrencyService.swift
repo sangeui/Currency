042b7154ada40af95d4d9e79386b1f31b75a7cdc
Sangeui <Sangeuis@icloud.com>
2021-01-21 18:11:27 +0900

    chore: added making URL codes in request(endpoint:queries:completion:)

--- Currency/Currency/Service/CurrencyService.swift
+++ Currency/Currency/Service/CurrencyService.swift
@@ -18,5 +18,23 @@ class CurrencyService {
     
     func request(endpoint: String, queries: [String: String], completion: @escaping CurrencyCompletion) {
         
+        guard let url = URL(string: endpoint) else {
+            completion(.failure(.invalidURL(endpoint)))
+            return
+        }
+        
+        guard var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
+            completion(.failure(.invalidURL(endpoint)))
+            return
+        }
+        
+        let queries = queries.compactMap({ URLQueryItem(name: $0.key, value: $0.value) })
+        
+        urlComponents.queryItems = queries
+        
+        guard let urlSuccessed = urlComponents.url else {
+            completion(.failure(.invalidURL(endpoint)))
+            return
+        }
     }
 }
