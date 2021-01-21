c1156751a83d177344a63f14578316841e8ce68a
Sangeui <Sangeuis@icloud.com>
2021-01-21 20:24:21 +0900

    test: added a failable test case, 'testCurrencyService_whenRequestedFailed_isNetworkErrorPassed'

--- Currency/CurrencyTests/ServiceTest/CurrencyServiceTests.swift
+++ Currency/CurrencyTests/ServiceTest/CurrencyServiceTests.swift
@@ -29,13 +29,10 @@ class CurrencyServiceTests: XCTestCase {
     }
     
     func testCurrencyService_whenInitialized_isInjectedSession() {
-        
         XCTAssertNotNil(sut.session)
-        
     }
     
     func testCurrencyService_whenRequested_isCorrectURL() {
-        
         sut.request(endpoint: endpoint, queries: queries) { result in
             switch result {
             case .failure(.invalidURL(_)): XCTAssert(false)
@@ -45,7 +42,6 @@ class CurrencyServiceTests: XCTestCase {
     }
     
     func testCurrencyService_whenRequested_isSuccess() {
-        
         let expectation = XCTestExpectation()
 
         let session = MockSession()
@@ -54,14 +50,31 @@ class CurrencyServiceTests: XCTestCase {
 
         let service = CurrencyService(session: session)
 
-        service.request(endpoint: endpoint, queries: queries) {
-            result in
+        service.request(endpoint: endpoint, queries: queries) { result in
             switch result {
             case .success(_): expectation.fulfill()
             case .failure(_): XCTAssert(false)
             }
         }
 
+        wait(for: [expectation], timeout: 10)
+    }
+    
+    func testCurrencyService_whenRequestedFailed_isNetworkErrorPassed() {
+        let expectation = XCTestExpectation()
+        
+        let session = MockSession()
+        session.populateError()
+        
+        let service = CurrencyService(session: session)
+        
+        service.request(endpoint: endpoint, queries: queries) { result in
+            switch result {
+            case .failure(.networkError(_)): expectation.fulfill()
+            default: return
+            }
+        }
+        
         wait(for: [expectation], timeout: 10)
     }
 }
