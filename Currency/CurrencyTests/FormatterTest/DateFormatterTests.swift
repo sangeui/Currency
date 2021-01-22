055b53947f474e730fb3737cb86b9ebdb1782296
Sangeui <Sangeuis@icloud.com>
2021-01-22 15:55:25 +0900

    test: passed the test 'testDate_whenConverted_isSuccessed'

--- Currency/CurrencyTests/FormatterTest/DateFormatterTests.swift
+++ Currency/CurrencyTests/FormatterTest/DateFormatterTests.swift
@@ -6,7 +6,17 @@
 //
 
 import XCTest
+@testable import Currency
 
 class DateFormatterTests: XCTestCase {
-    
+    var source: Double = 1611123846
+    var target = "2021-01-20 15:24"
+
+    func testDate_whenConverted_isSuccessed() {
+        let formatter = DateFormatter.currencyFormatter
+        let date = Date(timeIntervalSince1970: source)
+        let converted = formatter.string(from: date)
+        
+        XCTAssertEqual(converted, target)
+    }
 }
