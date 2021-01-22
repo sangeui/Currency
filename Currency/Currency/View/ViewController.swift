a8eb9694cb5b730d168c38a1dadebcc9d4959b6d
Sangeui <Sangeuis@icloud.com>
2021-01-23 01:16:26 +0900

    feat: added UIPickerView

--- Currency/Currency/View/ViewController.swift
+++ Currency/Currency/View/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var currencyPickerView: UIPickerView!
+    @IBOutlet weak var sendToLabel: UILabel!
+    
+    var list: [String: String]!
+    var orderedList: [(key: String, value: String)]!
+    var viewModel: CurrencyViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let service = CurrencyService(session: .shared)
+        viewModel = CurrencyViewModel(service: service)
+        
+        self.list = viewModel.list
+        self.orderedList = viewModel.list.sorted(by: >)
+        
+        currencyPickerView.delegate = self
+        currencyPickerView.dataSource = self
     }
 
-
+    @IBAction func showPickerView(_ sender: Any) {
+        currencyPickerView.isHidden = false
+    }
 }
 
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return orderedList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return orderedList[row].value
+    }
+}
+
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        sendToLabel.text = orderedList[row].value
+        pickerView.isHidden = true
+    }
+}
