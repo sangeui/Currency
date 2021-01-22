c297b95227011011538bb1b4d6d3086a194c444e
Sangeui <Sangeuis@icloud.com>
2021-01-22 16:21:52 +0900

    test: passed 'testNumberFormatter_whenFormatted_isSuccessed'

--- Currency/CurrencyTests/FormatterTest/NumberFormatterTests.swift
+++ Currency/CurrencyTests/FormatterTest/NumberFormatterTests.swift
@@ -6,26 +6,22 @@
 //
 
 import XCTest
+@testable import Currency
 
 class NumberFormatterTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    let remittance: Double = 100
+    let exchangeRate: Double = 1103.70
+    
+    let target = "110,370.00"
+    
+    func testNumberFormatter_whenFormatted_isSuccessed() {
+        let inAnotherCurrency = remittance * exchangeRate
+        let nsNumber = NSNumber(value: inAnotherCurrency)
+        let formatter = NumberFormatter.currencyFormatter
+        let formatted = formatter.string(from: nsNumber)
+        
+        XCTAssertNotNil(formatted)
+        XCTAssertEqual(formatted!, target)
     }
 }
