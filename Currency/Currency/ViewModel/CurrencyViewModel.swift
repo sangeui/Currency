86aea555294130c35bfe7555ef82491e269767b6
Sangeui <Sangeuis@icloud.com>
2021-01-23 16:12:44 +0900

    feat: added currencyRate method

--- Currency/Currency/ViewModel/CurrencyViewModel.swift
+++ Currency/Currency/ViewModel/CurrencyViewModel.swift
@@ -11,16 +11,21 @@ class CurrencyViewModel {
     var source = "USD"
     var destination = "KRW"
     
-    var list: [String:String] {
+    var list: [String: String] {
         let list = CurrencyList.allCases
             .filter({ $0.code != source })
             .filter({ $0.code != destination})
-            .reduce(into: [String: String]()) { $0[$1.code] = $1.description }
+            .reduce(into: [String: String]()) {
+                $0[$1.code] = $1.description
+            }
             
         return list
     }
     
     var service: CurrencyService
+    
+    let numberFormatter = NumberFormatter.currencyFormatter
+    let dateFormatter = DateFormatter.currencyFormatter
     
     init(service: CurrencyService) {
         self.service = service
@@ -32,6 +37,46 @@ class CurrencyViewModel {
     
     func changeDestination(to target: String) {
         self.destination = target
+    }
+    
+    func currencyRate(completion: @escaping ([String:String]?, String?) -> Void) {
+        let queries = [
+            "access_key": "",
+            "source": source,
+            "currencies": destination,
+            "format": "1"
+        ]
+        service.request(queries: queries) { [weak self] result in
+            
+            guard let self = self else { return }
+            
+            switch result {
+
+            case .success(let currency):
+                
+                // 예시: USDKRW
+                let rateKey = self.source + self.destination
+                
+                guard let rate = currency.destinations[rateKey]
+                else { print("❌ 수취 국가 환율 오류"); return }
+
+                guard let formattedRate = self.numberFormatter.string(from: rate)
+                else { print("❌ 환율 변환 오류"); return }
+                                
+                let description = "\(formattedRate) \(self.destination) / \(self.source)"
+                
+                let time = self.dateFormatter.string(from: currency.timestamp)
+                
+                let result = [
+                    "description": description,
+                    "time": time
+                ]
+                
+                completion(result, nil)
+
+            case .failure(let error): print(error.localizedDescription)
+            }
+        }
     }
 }
 
