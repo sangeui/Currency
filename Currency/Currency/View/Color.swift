63f4ea556664145aa68fbe74cd1c55a74fbd74fc
Sangeui <Sangeuis@icloud.com>
2021-01-24 16:28:11 +0900

    feat: added Color struct for the app's entire life cycle

---
+++ Currency/Currency/View/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  Currency
+//
+//  Created by 서상의 on 2021/01/24.
+//
+
+import UIKit
+
+struct Color {
+    static let text: UIColor = .label
+    static let error: UIColor = .systemRed
+}
