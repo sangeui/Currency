04dc10ba07d610cf7e17eab59fea799606500ada
Sangeui <sangeuis@icloud.com>
2021-02-25 22:50:50 +0900

    API Key 제거

--- Currency/Currency/Service/CurrencyAPI.swift
+++ Currency/Currency/Service/CurrencyAPI.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct CurrencyAPI {
-    static let key = "bae9d0e6ce4a4b885835497c7fe8f2f2"
+    static let key = "YOUR KEY"
     static let endpoint = "http://apilayer.net/api/live"
     
     static func queries(source: String, currencies: [String]) -> [String:String] {
